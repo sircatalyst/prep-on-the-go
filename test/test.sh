50211136761e1e02eee187baa7daf07cb7ebffb6
sircatalyst <temibami@gmail.com>
2020-07-29 07:55:12 +0100

    fix type issue

--- test/test.sh
+++ test/test.sh
@@ -11,7 +11,8 @@ fi
 export NODE_ENV=$env
 
 # In band with coverage
-jest --forceExit --runInBand
+# jest --forceExit --runInBand
+jest --forceExit --runInBand --coverage --forceExit
 #jest --forceExit --runInBand --coverage
 # jest test/examQuestion.e2e-spec  --runInBand --forceExit
 #--detectOpenHandles
