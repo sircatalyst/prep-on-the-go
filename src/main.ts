7898e6fd4336bfddec4614babb35c8947b22c918
sircatalyst <temibami@gmail.com>
2020-08-16 19:52:40 +0100

    increase request limit from 100 to 500 requests per 15mins

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,7 @@ async function bootstrap() {
 	app.use(
 		rateLimit({
 			windowMs: 15 * 60 * 1000, // 15 minutes
-			max: 100, // limit each IP to 100 requests per windowMs
+			max: 500, // limit each IP to 100 requests per windowMs
 			message:
 				"Too many requests from this IP, please try again after 15  mins"
 		})
